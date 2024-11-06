b5f595538fd9dee371e3436a4a89473858ba2da7
Gautham Krishna J L <gauthamkrishnalj@gmail.com>
2024-11-06 15:28:23 +0530

    Add files via upload

---
+++ Sorting_Algorithms/bubble_sort.py
@@ -0,0 +1,15 @@
+def bubble_sort(list1,length):
+    for i in range(0,length-1):
+        if(list1[i]>list1[i+1]):
+            list1[i],list1[i+1]=list1[i+1],list1[i]
+            bubble_sort(list1,length-1)
+        else:
+            bubble_sort(list1,length-1)
+
+list1=[]
+n=int(input("Enter how many elements needed"))
+for i in range(n):
+    list1.append(int(input("Enter an element")))
+length=len(list1)
+bubble_sort(list1,length)
+print(list1)
